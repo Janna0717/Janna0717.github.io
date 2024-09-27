b30217833b20f264fe9da09082e69e00d33d63c1
Janna0717 <janna57481@gmail.com>
2024-09-27 21:15:24 +0800

    將TOP改用jQuery功能完成
    加入jQuery核心
    加入all.js
    修改TOP的div為button
    將top加入回到頂端的js功能

---
+++ script/all.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // 當用戶滾動到一定距離時顯示按鈕
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 150) {
+            $('#scrollToTop').fadeIn(); // 使用淡入效果顯示按鈕
+        } else {
+            $('#scrollToTop').fadeOut(); // 使用淡出效果隱藏按鈕
+        }
+    });
+
+    // 點擊按鈕時平滑滾動到頁面頂部
+    $('#scrollToTop').click(function() {
+        $('html, body').animate({scrollTop: 0}, 700); // 700毫秒內滾動到頂部
+    });
+});
