f9218a2371de38e07b0ba3a8005caa23a7674bb2
Janna0717 <janna57481@gmail.com>
2024-10-02 16:45:34 +0800

    新增menu欄位及修正a連結
    新增一欄"部落格"
    新增"首頁""部落格"的跳轉連結
    新增判斷式 讓首頁及部落格可以正常跳轉

--- script/all.js
+++ script/all.js
@@ -15,6 +15,12 @@ $(document).ready(function() {
 
     // 折疊式選單
     $('.menu_list > ul > li > a').click(function(event) {
+            // 檢查是否點擊的是首頁或部落格的連結
+        if ($(this).text() === '首頁' || $(this).text() === '部落格') {
+            // 不做任何處理，讓連結正常運作
+            return; 
+        }
+
         event.preventDefault();
         $(this).parent().siblings().find('ul').slideUp(150);
         $(this).parent().find('ul').slideToggle(150);
